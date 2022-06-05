e3f3596a8d0c60f6543dc7c5d0d833c4daca7aca
jmr <raoult.jean_michel@univ.teluq.ca>
2022-06-05 14:36:05 -0400

    Effacement du script de prototypage
    L'approche est intégrée dans le script de test

--- Scripts/Windows/runme.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [int]$coeurs,
-    [int]$memoire
-)
-
-Write-Output $coeurs
-Write-Output $memoire
-Write-Output $env:NUMBER_OF_PROCESSORS
-systeminfo | select-string 'Total Physical Memory'
