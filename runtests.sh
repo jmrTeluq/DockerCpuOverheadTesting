d6a1b56414c1d05f0495f1e3cb173cd66636fff7
jm <raoult.jean_michel@univ.teluq.ca>
2022-06-09 22:29:58 -0400

    Mise à jour du fichier d'exécution de tests
    Ajout d'un accès aux paramètres de test au lieu d'utiliser une valeur par défaut
    Réorganisation des paramètres et des commentaires
    Ajout d'une fonction de nettoyage pour transférer les résultats du test sur les flottants dans un seul fichier et éliminer les fichiers de résultats individuels et le fichier de validation pour le même test

--- runtests.sh
+++ runtests.sh
@@ -33,25 +33,93 @@ Aide(){
     echo ""
 }
 
-########### Valeurs par défaut des paramètres ###################
+########################## Fonction de nettoyage ###############################
+Nettoyage(){
+    
+    # Nettoyage des fichiers du test sur les flottants
 
-# Nombre de cœurs de processeur à utiliser
-# nproc serait une solution plus simple, mais il compte les cœurs logiques seulement
-# et certains processeurs n'ont pas deux cœurs logiques par cœur physique
-# ce qui rendrait la simple opération de division par 2 inadéquate
-coeurs=${coeurs:-$(nproc)}
-# Quantité de mémoire vive à assigner au conteneur
-memoire=${memoire:-8}
-# Booléen contrôlant la construction de l'image Docker
-construire=${construire:-false}
-# Booléen contrôlant l'affichage d'un menu d'aide
-help=${help:-false}
-# Booléen contrôlant l'exécution des tests natifs
-testsNatif=${testsNatif:-false}
-# Booléen contrôlant l'exécution des tests Docker
-testsConteneur=${testsConteneur:-false}
-# Booléen contrôlant l'exécution des tests natifs et Docker
-tests=${tests:-false}
+    # Compilation des temps d'exécution dans un seul fichier texte
+    grep -hroP "Total Computation Time:[\s]+\K([\d]+\.[\d]+)" "$1" > "Flottants.txt"
+    # Transfert du fichier synthèse dans le dossier de résultats
+    mv "Flottants.txt" "$1/Flottants.txt"
+    # Effacement des fichiers de résultats individuels et du fichier de vérification
+    find "$1" -type f -name 'Pi*.txt' -delete
+}
+
+####################### Valeurs par défaut des tests ###########################
+
+    # Paramètre permettant l'exécution de tous les type de tests (à ne pas confondre
+    # avec le paramètre tests qui controle l'environnement où ces tests seront
+    # exécuté)
+    toutTests=${toutTests:-false}
+
+
+########################### Test pour entiers ##################################
+
+    # Booléen déterminant si le test sur les entiers sera effectué
+    testEntiers=${testEntiers:-false}
+    # Booléen déterminant si le test d'entiers sera multicœur
+    multicoeurEntier=${multicoeurEntier:-true}
+    # Limite supérieure des nombres premiers recherchés (forme: 10e9)
+    entierLimite=${entierLimite:-"10e9"}
+    # Nombre entier de répétitions du test pour obtenir une valeur moyenne
+    entierRepetitions=${entierRepetitions:-10}
+    # Nombre entier d'itérations du test utilisées comme réchauffement
+    entierRechauffement=${entierRechauffement:-5}
+
+########################## Test pour flottants #################################
+
+    # Booléen déterminant si le test sur les flottants sera effectué
+    testFlottants=${testFlottants:-false}
+    # Booléen déterminant si le test pour les flottants sera multicœur
+    multicoeurFlottants=${multicoeurFlottants:-true}
+    # Définition du nombre de chiffres de Pi à calculer (25m, 50m, 100m, 250m, 
+    # 500m,1b sont les tailles disponibles avant d'excéder une mémoire vive de 4GB)
+    flottantLimite=${flottantLimite:-"25m"}
+    # Définition du nombre de répétitions à effectuer pour le réchauffement
+    flottantRechauffement=${flottantRechauffement:-2}
+    # Définition du nombre de répétitions à effectuer pour le test
+    flottantRepetitions=${flottantRepetitions:-1}
+
+############################## Test mémoire ####################################
+
+    # Booléen déterminant si le test sur la mémoire sera effectué
+    testMemoire=${testMemoire:-false}
+    # Nombre de variations de taille de problèmes calculées
+    memoireNbrTests=${memoireNbrTests:-2}
+    # Liste des tailles de problèmes calculées
+    memoireTaille=${memoireTaille:-"1000 2000"}
+    # Liste des dimensions correspondantes aux tailles des problèmes
+    memoireDimension=${memoireDimension:-"1000 2000"}
+    # Liste du nombre de répétitions à calculer pour chaque problème
+    memoireRepetition=${memoireRepetition:-"10 10"}
+    # Alignement mémoire pour chaque problème
+    memoireAlignement=${memoireAlignement:-"4 4"}
+
+##################### Paramètre de construction d'image ########################
+
+    # Booléen contrôlant la construction de l'image Docker
+    construire=${construire:-false}
+    # Nombre de cœurs de processeur à utiliser
+    # nproc serait une solution plus simple, mais il compte les cœurs logiques seulement
+    # et certains processeurs n'ont pas deux cœurs logiques par cœur physique
+    # ce qui rendrait la simple opération de division par 2 inadéquate
+    coeurs=${coeurs:-$(nproc)}
+    # Quantité de mémoire vive à assigner au conteneur
+    memoire=${memoire:-8}
+
+########################## Paramètres utilitaires ##############################
+
+    # Paramètre contrôlant l'affichage du menu d'aide
+    help=${help:-false}    
+    
+    # Paramètre controlant l'exécution des tests dans l'environnement natif
+    testsNatif=${testsNatif:-false}
+    # Paramètre controlant l'exécution des tests dans l'environnement Docker
+    testsConteneur=${testsConteneur:-false}
+    # Paramètre permettant l'exécution séquentielle des tests dans l'environnement
+    # natif puis dans l'environnement Docker
+    tests=${tests:-false}
 
 # Les scripts bash n'ont pas de fonctionnalités natives similaires à PowerShell
 # en ce qui concerne les arguments nommés. On peut toutefois émuler cette
@@ -112,7 +180,25 @@ fi
 # Exécution des tests natifs
 if [[ $testsNatif != "false" || $tests != "false" ]]; then
     ./Scripts/Linux/testsNatifs.sh \
-    --toutTests true
+    --testEntiers $testEntiers \
+    --multicoeurEntier $multicoeurEntier \
+    --entierLimite "$entierLimite" \
+    --entierRepetitions $entierRepetitions \
+    --entierRechauffement $entierRechauffement \
+    --testFlottants $testFlottants \
+    --multicoeurFlottants $multicoeurFlottants \
+    --flottantLimite "$flottantLimite" \
+    --flottantRechauffement $flottantRechauffement \
+    --flottantRepetitions $flottantRepetitions \
+    --testMemoire $testMemoire \
+    --memoireNbrTests $memoireNbrTests \
+    --memoireTaille "$memoireTaille" \
+    --memoireDimension "$memoireDimension" \
+    --memoireRepetition "$memoireRepetition" \
+    --memoireAlignement "$memoireAlignement" \
+    --toutTests $toutTests
+
+    Nettoyage "Resultats/6 - Linux (Natif)"
 fi
 
 # Exécution d'un conteneur Docker avec l'image construite
@@ -125,5 +211,23 @@ if [[ $testsConteneur != "false" || $tests != "false" ]]; then
     -it \
     jmrteluq/linuxtests \
     ./testsConteneur.sh \
-    --toutTests true
+    --testEntiers $testEntiers \
+    --multicoeurEntier $multicoeurEntier \
+    --entierLimite "$entierLimite" \
+    --entierRepetitions $entierRepetitions \
+    --entierRechauffement $entierRechauffement \
+    --testFlottants $testFlottants \
+    --multicoeurFlottants $multicoeurFlottants \
+    --flottantLimite "$flottantLimite" \
+    --flottantRechauffement $flottantRechauffement \
+    --flottantRepetitions $flottantRepetitions \
+    --testMemoire $testMemoire \
+    --memoireNbrTests $memoireNbrTests \
+    --memoireTaille "$memoireTaille" \
+    --memoireDimension "$memoireDimension" \
+    --memoireRepetition "$memoireRepetition" \
+    --memoireAlignement "$memoireAlignement" \
+    --toutTests $toutTests
+
+    Nettoyage "Resultats/7 - Linux (Docker)"
 fi
